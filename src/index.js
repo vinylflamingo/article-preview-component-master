05d679e29e94dc5599d4c4c9b9b2c60e1cf6a9c5
vinylflamingo <fcostoyaprograms@gmail.com>
2021-04-01 11:12:14 -0400

    challenge completed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 const shareButton = document.getElementById('shareButton');
-const shareArrow = document.getElementById('shareArrow')
-const shareMenu = document.getElementById('shareMenu')
+const shareArrow = document.getElementById('shareArrow');
+const shareMenu = document.getElementById('shareMenu');
+
+
+
 
 function activate() {
+    console.log("share menu toggled")
     if (shareMenu.classList.contains('active')){
         shareButton.classList.remove('btn-active');
         shareArrow.classList.remove('arw-active');
+        shareArrow.classList.add('fade-out');
         shareMenu.classList.remove('active');
+        
     } else {
         shareButton.classList.add('btn-active');
         shareArrow.classList.add('arw-active');
+        shareMenu.classList.add('fade-in');
         shareMenu.classList.add('active');
     }
 }
